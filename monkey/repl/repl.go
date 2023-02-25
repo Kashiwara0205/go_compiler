b381eab7d68398348bda27ac0da207da50792713
kashiwara0205 <tamatebako0205@gmail.com>
2023-02-25 12:10:58 +0900

    Add repl

---
+++ monkey/repl/repl.go
@@ -0,0 +1,57 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkey/compiler"
+	"monkey/lexer"
+	"monkey/parser"
+	"monkey/vm"
+)
+
+const PROMPT = ">"
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			fmt.Print("parseError")
+		}
+
+		comp := compiler.New()
+		err := comp.Compile(program)
+
+		if err != nil {
+			fmt.Fprintf(out, "Woops! compilation failed:\n %s\n", err)
+			continue
+		}
+
+		machine := vm.New(comp.Bytecode())
+		err = machine.Run()
+
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Excuting bytecode failed:\n %s\n", err)
+			continue
+		}
+
+		stackTop := machine.StackTop()
+		io.WriteString(out, stackTop.Inspect())
+		io.WriteString(out, "\n")
+	}
+
+}
