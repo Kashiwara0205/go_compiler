b84f71a23de29cdf39ffdf82382473bf444a5a7f
kashiwara0205 <tamatebako0205@gmail.com>
2023-03-19 12:27:10 +0900

    Add if to vm

--- monkey/vm/vm.go
+++ monkey/vm/vm.go
@@ -148,6 +148,15 @@ func (vm *VM) StackTop() object.Object {
 	return vm.stack[vm.sp-1]
 }
 
+func isTruthy(obj object.Object) bool {
+	switch obj := obj.(type) {
+	case *object.Boolean:
+		return obj.Value
+	default:
+		return true
+	}
+}
+
 func (vm *VM) Run() error {
 	for ip := 0; ip < len(vm.instructions); ip++ {
 		op := code.Opcode(vm.instructions[ip])
@@ -160,6 +169,18 @@ func (vm *VM) Run() error {
 			err := vm.push(vm.constants[constIndex])
 			if err != nil {
 				return err
+			}
+		case code.OpJump:
+			pos := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip = pos - 1
+
+		case code.OpJumpNotTruthy:
+			pos := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip += 2
+			condition := vm.pop()
+
+			if !isTruthy(condition) {
+				ip = pos - 1
 			}
 		case code.OpPop:
 			vm.pop()
