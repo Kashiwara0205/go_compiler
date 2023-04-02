eab44125ebda2fe0cd46183a1e0a98f8ebb57fe8
kashiwara0205 <tamatebako0205@gmail.com>
2023-04-02 14:07:50 +0900

    stringで足し算できるようにした

--- monkey/vm/vm_test.go
+++ monkey/vm/vm_test.go
@@ -74,6 +74,20 @@ func runVmTests(t *testing.T, tests []vmTestCase) {
 	}
 }
 
+func testStringObject(expected string, actual object.Object) error {
+	result, ok := actual.(*object.String)
+
+	if !ok {
+		return fmt.Errorf("object is not String. got=%T (%+v)", actual, actual)
+	}
+
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. got=%q, want=%q", result.Value, expected)
+	}
+
+	return nil
+}
+
 func testExpectedObject(
 	t *testing.T,
 	expected interface{},
@@ -93,6 +107,12 @@ func testExpectedObject(
 		err := testBooleanObject(bool(expected), actual)
 		if err != nil {
 			t.Errorf("testBooleanObject failed: %s", err)
+		}
+
+	case string:
+		err := testStringObject(expected, actual)
+		if err != nil {
+			t.Errorf("testsStringObject failed: %s", err)
 		}
 
 	case *object.Null:
@@ -180,6 +200,16 @@ func TestGlobalLetStatements(t *testing.T) {
 		{"let one = 1; one", 1},
 		{"let one = 1; let two = 2; one + two", 3},
 		{"let one = 1; let two = one + one; one + two", 3},
+	}
+
+	runVmTests(t, tests)
+}
+
+func TestStringExpressions(t *testing.T) {
+	tests := []vmTestCase{
+		{`"monkey"`, "monkey"},
+		{`"mon" + "key"`, "monkey"},
+		{`"mon" + "key" + "banana"`, "monkeybanana"},
 	}
 
 	runVmTests(t, tests)
