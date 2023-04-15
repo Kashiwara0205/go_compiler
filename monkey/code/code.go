c267b614005698317e96d2f0f815ac36dbe0bba8
kashiwara0205 <tamatebako0205@gmail.com>
2023-04-15 15:42:20 +0900

    codeにlocal変数の概念追加

--- monkey/code/code.go
+++ monkey/code/code.go
@@ -75,6 +75,8 @@ const (
 	OpCall
 	OpReturnValue
 	OpReturn
+	OpGetLocal
+	OpSetLocal
 )
 
 type Definition struct {
@@ -107,6 +109,8 @@ var definitions = map[Opcode]*Definition{
 	OpCall:          {"OpCall", []int{}},
 	OpReturnValue:   {"OpReturnValue", []int{}},
 	OpReturn:        {"OpReturn", []int{}},
+	OpGetLocal:      {"OpGetLocal", []int{1}},
+	OpSetLocal:      {"OpSetLocal", []int{1}},
 }
 
 func Lookup(op byte) (*Definition, error) {
@@ -142,6 +146,9 @@ func Make(op Opcode, operands ...int) []byte {
 				instrction[offset:],
 				uint16(o),
 			)
+
+		case 1:
+			instrction[offset] = byte(o)
 		}
 
 		offset += width
@@ -158,12 +165,19 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 		switch width {
 		case 2:
 			operands[i] = int(ReadUint16(ins[offset:]))
+
+		case 1:
+			operands[i] = int(ReadUint8(ins[offset:]))
 		}
 
 		offset += width
 	}
 
 	return operands, offset
+}
+
+func ReadUint8(ins Instructions) uint8 {
+	return uint8(ins[0])
 }
 
 func ReadUint16(ins Instructions) uint16 {
--- monkey/code/code_test.go
+++ monkey/code/code_test.go
@@ -15,6 +15,11 @@ func TestReardOperands(t *testing.T) {
 			[]int{65535},
 			2,
 		},
+		{
+			OpGetLocal,
+			[]int{255},
+			1,
+		},
 	}
 
 	for _, tt := range tests {
@@ -44,14 +49,16 @@ func TestReardOperands(t *testing.T) {
 func TestInstructionsString(t *testing.T) {
 	instructions := []Instructions{
 		Make(OpAdd),
+		Make(OpGetLocal, 1),
 		Make(OpConstant, 2),
 		Make(OpConstant, 65535),
 	}
 
 	expected := `
 	0000 OpAdd
-	0001 OpConstant 2
-	0004 OpConstant 65535`
+	0001 OpGetLocal 1
+	0003 OpConstant 2
+	0006 OpConstant 65535`
 
 	concatted := Instructions{}
 
@@ -79,6 +86,11 @@ func TestMake(t *testing.T) {
 			OpAdd,
 			[]int{},
 			[]byte{byte(OpAdd)},
+		},
+		{
+			OpGetLocal,
+			[]int{255},
+			[]byte{byte(OpGetLocal), 255},
 		},
 	}
 
